360d792c6d7ada35fda91471d0d59bb83179b02f
gabchak <andrey.gabchak@gmail.com>
2018-10-30 17:25:11 +0200

    New model

---
+++ src/main/java/com/gabchak/model/Category.java
@@ -0,0 +1,47 @@
+package com.gabchak.model;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public class Category {
+
+    private Long id;
+    @NotNull
+    private String name;
+    private List<Product> products;
+
+    public Category(Long id, String name, List<Product> products) {
+        this.id = id;
+        this.name = name;
+        this.products = products;
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+}
