e5724b3ee32f278a4001b2f0906a9c289ae0798d
gabchak <andrey.gabchak@gmail.com>
2018-10-31 18:07:01 +0200

    Create product service

---
+++ src/main/java/com/gabchak/service/ProductServiceImpl.java
@@ -0,0 +1,45 @@
+package com.gabchak.service;
+
+import com.gabchak.dao.ProductDao;
+import com.gabchak.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductDao productDao;
+
+    @Autowired
+    public ProductServiceImpl(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        productDao.addProduct(product);
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return productDao.findById(id);
+    }
+
+    @Override
+    public void update(Product product) {
+        productDao.update(product);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productDao.deleteById(id);
+    }
+
+    @Override
+    public Optional<List<Product>> findAll() {
+        return productDao.findAll();
+    }
+}
