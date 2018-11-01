401456e2709dda5ac346e1d311b2b743a9836d48
gabchak <andrey.gabchak@gmail.com>
2018-11-01 16:04:13 +0200

    Added constructor with id and name only.

--- src/main/java/com/gabchak/model/Category.java
+++ src/main/java/com/gabchak/model/Category.java
@@ -16,6 +16,11 @@ public class Category {
         this.products = products;
     }
 
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Category() {
     }
 
