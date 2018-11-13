e5856425820d71be2474c2250472041d13b43cf6
andrey-gabchak <andrey.gabchak@gmail.com>
2018-11-13 20:38:12 +0200

    Create order dao interface

---
+++ src/main/java/com/gabchak/dao/OrderDao.java
@@ -0,0 +1,20 @@
+package com.gabchak.dao;
+
+
+import com.gabchak.model.Order;
+
+import java.util.List;
+
+public interface OrderDao {
+
+    void create(Order order);
+
+    void update(Order order);
+
+    void delete(Long id);
+
+    Order findById(Long id);
+
+    List<Order> findAll();
+
+}
