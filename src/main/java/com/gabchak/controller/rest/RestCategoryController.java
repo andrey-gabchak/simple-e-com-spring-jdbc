e5624a64edd8d87dc02b01613817fffe4a8198af
gabchak <andrey.gabchak@gmail.com>
2018-11-19 20:44:17 +0200

    added cross origin

--- src/main/java/com/gabchak/controller/rest/RestCategoryController.java
+++ src/main/java/com/gabchak/controller/rest/RestCategoryController.java
@@ -4,6 +4,7 @@ import com.gabchak.controller.external.model.CategoryDto;
 import com.gabchak.model.Category;
 import com.gabchak.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class RestCategoryController {
         this.categoryService = categoryService;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("/rest/categories")
     public List<CategoryDto> getAll() {
         return categoryService.findAll().stream()
