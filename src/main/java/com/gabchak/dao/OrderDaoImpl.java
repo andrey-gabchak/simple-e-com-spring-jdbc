f32ced90d3f2f18ca628296cdd5b9bae1390bfe6
gabchak <andrey.gabchak@gmail.com>
2018-11-20 17:21:16 +0200

    Refactored 'create' method using Map<Product, Quantity>

--- src/main/java/com/gabchak/dao/OrderDaoImpl.java
+++ src/main/java/com/gabchak/dao/OrderDaoImpl.java
@@ -1,18 +1,17 @@
 package com.gabchak.dao;
 
 import com.gabchak.model.Order;
+import com.gabchak.model.Product;
 import com.gabchak.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
+import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Repository
 public class OrderDaoImpl implements OrderDao {
@@ -56,13 +55,17 @@ public class OrderDaoImpl implements OrderDao {
                 }
 
                 if (orderId != null) {
-                    for (int i = 0; i < order.getProducts().size(); i++) {
+
+                    for (Map.Entry<Product, Integer> entry : order.getProducts().entrySet()) {
+                        Product product = entry.getKey();
+                        Integer quantity = entry.getValue();
+
                         productsStatement = connection.prepareStatement(
                                 "INSERT INTO ORDER_TO_PRODUCT (FK_ORDER_ID, FK_PRODUCT_ID, PRODUCT_QUANTITY) VALUES (?, ?, ?)");
                         productsStatement.setLong(1, orderId);
-                        long productId = order.getProducts().get(i).getId();
-                        productsStatement.setLong(2, productId);
-                        productsStatement.setInt(3, order.getProductsQuantity().get(productId));
+                        productsStatement.setLong(2, product.getId());
+                        productsStatement.setInt(3, quantity);
+
                         productsStatement.executeUpdate();
                     }
                 }
@@ -108,7 +111,7 @@ public class OrderDaoImpl implements OrderDao {
                                 "UPDATE ORDER_TO_PRODUCT SET FK_PRODUCT_ID = ?, PRODUCT_QUANTITY = ? WHERE FK_ORDER_ID = ?");
                         Long id = order.getProducts().get(i).getId();
                         orderToProductStatement.setLong(1, id);
-                        orderToProductStatement.setInt(2, order.getProductsQuantity().get(id));
+                        orderToProductStatement.setInt(2, order.getProducts().get(id));
                         orderToProductStatement.setLong(3, order.getOrderId());
                         orderToProductStatement.executeUpdate();
                     }
@@ -218,20 +221,16 @@ public class OrderDaoImpl implements OrderDao {
     }
 
     private Order getProductAndQuantity(Order order) {
-        Map<Long, Integer> quantity = new HashMap<>();
+        Map<Product, Integer> products = new HashMap<>();
         Long orderId = order.getOrderId();
+
         jdbcTemplate.query("SELECT FK_PRODUCT_ID, PRODUCT_QUANTITY FROM ORDER_TO_PRODUCT WHERE FK_ORDER_ID = ?",
-                new Object[]{orderId}, (rs, rowNum) -> quantity.put(
-                        rs.getLong("FK_PRODUCT_ID"),
+                new Object[]{orderId}, (rs, rowNum) -> products.put(
+                        productDao.findById(rs.getLong("FK_PRODUCT_ID")),
                         rs.getInt("PRODUCT_QUANTITY")
                 ));
 
-        Set<Long> keys = quantity.keySet();
-        for (Long key : keys) {
-            order.addProduct(productDao.findById(orderId));
-        }
-
-        order.setProductsQuantity(quantity);
+        order.setProducts(products);
         return order;
     }
 }
