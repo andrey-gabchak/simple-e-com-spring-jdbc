8bd0d3e3de977d7c59ff05bf97d5e322104a7f5c
gabchak <andrey.gabchak@gmail.com>
2018-11-20 16:31:03 +0200

    Create method 'findAllUsersProducts'

--- src/main/java/com/gabchak/dao/CartDaoImpl.java
+++ src/main/java/com/gabchak/dao/CartDaoImpl.java
@@ -1,7 +1,11 @@
 package com.gabchak.dao;
 
+import com.gabchak.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CartDaoImpl implements CartDao {
 
@@ -21,5 +25,19 @@ public class CartDaoImpl implements CartDao {
                 productId,
                 quantity);
     }
+
+
+    @Override
+    public Map<Product, Integer> findAllUsersProducts(Long userId) {
+        Map<Product, Integer> quantity = new HashMap<>();
+
+        jdbcTemplate.query("SELECT FK_PRODUCT_ID, PRODUCT_QUANTITY FROM CARTS WHERE FK_USER_ID = ?",
+                new Object[] {userId}, (rs, rowNum) -> quantity.put(
+                        productDao.findById(rs.getLong(1)),
+                        rs.getInt(2)
+                ));
+        return quantity;
+    }
+
 
 }
